9054240e7bc4f43f1ab6e4e34fada9246f2b8c9b
Jaivardhan Kapoor <kapoorj1@cs-046.org.aalto.fi>
2019-06-12 16:41:10 +0300

    add sgld, preliminary inference using GP mean function

--- samplers/sgld.py
+++ samplers/sgld.py
@@ -23,13 +23,15 @@ class MALA(Sampler):
 
     def step(self):
         """
-        Returns the sample, negative log-posterior w.r.t the old parameters if a closure is provided, and whether the sample is accepted or rejected
+        Proposal step
         """
 
         for group in self.param_groups:
             for p in group['params']:
                 if p.grad is None:
                     continue # assuming only Parameter objects are posterior vars
+                if torch.isnan(p.data).sum() or torch.isnan(p.data).sum():
+                    raise ValueError("Encountered NaN/Inf in parameter")
                 self.state[p]['data'] = p.data
                 self.state[p]['grad'] = p.grad.data
                 d_p = p.grad.data
@@ -66,10 +68,15 @@ class MALA(Sampler):
             # adding log-ratio of proposal
             for group in self.param_groups:
                 for p in group['params']:
+                    if torch.isnan(p.data).sum() or torch.isnan(p.data).sum():
+                        raise ValueError("Encountered NaN/Inf in parameter")
                     # reverse proposal prob
-                    log_alpha += -1./(4*group['lr'])*(self.state[p]['data'] - p.data + group['lr']*p.grad.data).pow(2).sum()
+                    # print(self.state[p]['data'])
+                    log_alpha += -1./(4*group['lr'])*(self.state[p]['data']\
+                                 - p.data + group['lr']*p.grad.data).pow(2).sum()
                     # forward proposal prob
-                    log_alpha -= -1./(4*group['lr'])*(p.data - self.state[p]['data'] + group['lr']*self.state[p]['grad']).pow(2).sum()
+                    log_alpha -= -1./(4*group['lr'])*(p.data - self.state[p]['data']\
+                                 + group['lr']*self.state[p]['grad']).pow(2).sum()
 
             if torch.isfinite(log_alpha) and torch.log(torch.rand(1)) < log_alpha:
                 pass
@@ -91,30 +98,121 @@ class MALA(Sampler):
 
         print("Burn-in phase started")
         for i in range(burn_in):
-            # print('Burn-in iter {}'.format(i+1))
-            self.zero_grad()
+            print('Burn-in iter {}'.format(i+1))
             self.loss = closure()
             # print(self.loss)
 
             # print('Loss: {}'.format(self.loss))
             self.loss.backward()
             self.step()
+            #### Checking if rejection works
+            param_prev = self.param_groups[0]['params'][0]
+            self.zero_grad()
             params, is_accepted = self.accept_or_reject(closure)
-            if not np.isfinite(params[0][1][0]).all():
-                exit()
-        
+            if not is_accepted:
+                if not torch.eq(self.param_groups[0]['params'][0].data, param_prev.data).all():
+                    raise RuntimeError("Rejection step copying does not work")
+            #####
+            
         print("Sampling phase started")
         for i in range(num_samples):
+            print('Sample iter: {}'.format(i+1))
             self.zero_grad()
             self.loss = closure()
             self.loss.backward()
             self.step()
-            chain += [self.accept_or_reject(closure)]
-            # print(self.loss)
-            logp_array += [-(self.loss.item())]
+            chain.append(self.accept_or_reject(closure))
+            logp_array.append(-(self.loss.item()))
         
         return chain, logp_array
 
 
+
 class SGLD(Sampler):
-    pass
+
+    """
+    For reference, look at
+    https://www.ics.uci.edu/~welling/publications/papers/stoclangevin_v6.pdf
+
+    Requires the following parameters during initialization:
+    1. parameters for optimization
+    2. learning rate params: lr0, gamma, t0, alpha
+    learning rate for each step is given by lr0*(t0+alpha*t)^(-gamma)
+    """
+
+    def __init__(self, params, **kwargs):
+        defaults = kwargs
+
+        if 'add_noise' not in defaults:
+            defaults['add_noise'] = True
+        super().__init__(params, defaults)
+        
+        self.logp = None
+
+
+    def step(self, lr=None):
+        """
+        Proposal step
+        """
+
+        for group in self.param_groups:
+            if lr:
+                group['lr'] = lr
+            for p in group['params']:
+                if p.grad is None:
+                    continue # assuming only Parameter objects are posterior vars
+                if torch.isnan(p.data).sum() or torch.isnan(p.data).sum():
+                    raise ValueError("Encountered NaN/Inf in parameter")
+                self.state[p]['data'] = p.data
+                self.state[p]['grad'] = p.grad.data
+                d_p = p.grad.data
+                # print("Gradient: {}".format(p.grad.data))
+                size = d_p.size()
+                if np.isnan(p.data).any():
+                    exit()
+                if group['add_noise']: 
+                    langevin_noise = Normal(
+                        torch.zeros(size),
+                        torch.ones(size) / np.sqrt(0.5*group['lr'])
+                    )
+                    p.data.add_(-group['lr'],
+                                d_p + langevin_noise.sample())
+                else:
+                    p.data.add_(-group['lr'],
+                                d_p)
+
+    def sample(self, closure, num_samples=1000, burn_in=100):
+        chain = self.samples
+        logp_array = []
+
+        def get_lr(t):
+            lr0 = self.param_groups[0]['lr0']
+            gamma = self.param_groups[0]['gamma']
+            t0 = self.param_groups[0]['t0']
+            alpha = self.param_groups[0]['alpha']
+            return lr0/np.power(t0+alpha*t, gamma)
+            
+
+        print("Burn-in phase started")
+        for i in range(burn_in):
+            print('Burn-in iter {}'.format(i+1))
+            self.loss = closure()
+            # print(self.loss)
+            # print('Loss: {}'.format(self.loss))
+            self.loss.backward()
+            self.step(lr=get_lr(i))
+            logp_array.append(-self.loss.item())
+            
+        print("Sampling phase started")
+        for i in range(num_samples):
+            print('Sample iter: {}'.format(i+1))
+            self.zero_grad()
+            self.loss = closure()
+            self.loss.backward()
+            self.step(lr=get_lr(i+burn_in))
+            params = [[p.clone().detach().data.numpy() for p in group['params']]
+                  for group in self.param_groups]
+            chain.append((params, True))
+            logp_array.append(-self.loss.item())
+        
+        return chain, logp_array
